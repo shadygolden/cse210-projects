b7a71113a3782bb2d3e42319874a117441840868
shadygolden <shadrackchineduuzoka@gmail.com>
2026-06-08 01:38:47 +0100

    add edited file

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,32 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {GetType().Name} ({_minutes} min): Distance {GetDistance():F1} km, Speed {GetSpeed():F1} kph, Pace {GetPace():F2} min per km";
+    }
+}
