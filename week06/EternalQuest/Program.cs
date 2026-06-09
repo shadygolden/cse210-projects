d5e42cdcc1330d39a52d05afc0359545721ea63f
shadygolden <shadrackchineduuzoka@gmail.com>
2026-06-09 14:42:40 +0100

    add edited file

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,55 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        /*
+        Creativity:
+        Added a running score system that is saved
+        and loaded with goals. Eternal goals display
+        with an infinity symbol [∞].
+        */
+
+        GoalManager manager = new GoalManager();
+
+        int choice = 0;
+
+        while (choice != 6)
+        {
+            manager.DisplayScore();
+
+            Console.WriteLine("1. Create Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Record Event");
+            Console.WriteLine("4. Save Goals");
+            Console.WriteLine("5. Load Goals");
+            Console.WriteLine("6. Quit");
+
+            Console.Write("Select Choice: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    manager.CreateGoal();
+                    break;
+
+                case 2:
+                    manager.ListGoals();
+                    break;
+
+                case 3:
+                    manager.RecordEvent();
+                    break;
+
+                case 4:
+                    manager.SaveGoals();
+                    break;
+
+                case 5:
+                    manager.LoadGoals();
+                    break;
+            }
+        }
     }
 }
